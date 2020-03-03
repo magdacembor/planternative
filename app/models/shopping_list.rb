125ef6d3db4a061c55842fcbc7dbabd7e0147f11
Alim Lalji <alimlalji1@gmail.com>
2020-03-03 12:22:45 +0000

    Validations added for shopping list model

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -2,4 +2,6 @@ class ShoppingList < ApplicationRecord
   belongs_to :user
   has_many :quantities
 
+  validates: :mark_as_done, inclusion: { in: [true, false] }
+  validates: :date, presence: true
 end
