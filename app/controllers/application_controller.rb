057356166233d03a5a16a987f15cc5b039b8d111
Piotr Damer <piotrd@czuby.net>
2020-03-03 14:53:41 +0000

    Updated the User new and edit forms to accomodate additional fields and cloudinary image upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :permit_params, if: :devise_controller?
+
+  def permit_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :avatar])
+  end
 end
