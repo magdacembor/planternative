62773e49090a284af9f2a2fca6e7a24455e6efdd
Alim Lalji <alimlalji1@gmail.com>
2020-03-03 15:26:21 +0000

    Updated validations for substitution model

--- app/models/substitution.rb
+++ app/models/substitution.rb
@@ -1,5 +1,7 @@
 class Substitution < ApplicationRecord
   belongs_to :meal
   belongs_to :product
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :meal, :product, presence: true
+  validates :name, uniqueness: { scope: :meal }
 end
