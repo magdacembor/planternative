1d1efcf05ad0ef51f4222df2b86761fe99033dc1
Alim Lalji <alimlalji1@gmail.com>
2020-03-03 14:13:40 +0000

    Validations added to quantity model

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -1,4 +1,7 @@
 class Quantity < ApplicationRecord
   belongs_to :product
   belongs_to :shopping_list
+
+  validates :quantity, presence: true, numericality: { only_integer: true}
+  validates :quantity, numericality: { greater_than: 0 }
 end
