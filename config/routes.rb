654a2ccb938e817f7cab93d9bb42e7d56311f81c
Magda Cembor <magda.cembor@gmail.com>
2020-03-03 21:36:09 +0000

    adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products
+  resources :products do
+    resources :reviews, only: [:show, :new, :create]
+  end
+  resources :shopping_lists, only: [:show, :update, :destroy]
+  resources :reviews, only: :destroy
+  get 'myprofile', to: 'pages#myprofile', as: :myprofile
 end
