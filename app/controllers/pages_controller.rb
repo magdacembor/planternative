b05d2aa04fcdd61ea1050feaca4493bc6fd74521
Katrina Au <katrinaau95@gmail.com>
2020-03-04 12:54:14 +0000

    Profile controller and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def myprofile
+    @shopping_lists = current_user.shopping_lists
+    @reviews = current_user.reviews
+    @products = current_user.products
   end
 
   def home; end
