6dcd10544b95a74c6ac7286c7be631ffe9339a16
Magda Cembor <magda.cembor@gmail.com>
2020-03-04 12:54:13 +0000

    shopping list start

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,2 +1,29 @@
 class ShoppingListsController < ApplicationController
+  before_action :find_list, only: [ :show, :update, :destroy ]
+
+  def show; end
+
+  def create
+    @shopping_list = ShoppingList.new
+    @shopping_list.date = Date.today
+    @shopping_list.user = current_user
+    if @shopping_list.save
+      redirect_to shopping_list_path(@shopping_list)
+    else
+      redirect_to home_path
+    end
+  end
+
+  def update
+    @shopping_list
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_list
+    @shopping_list = ShoppingList.find(params[:id])
+  end
 end
