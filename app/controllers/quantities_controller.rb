40b84a6077c976a35cc70f8d7d1278f7609e0fcc
Magda Cembor <magda.cembor@gmail.com>
2020-03-05 11:05:27 +0000

    shopping list

--- app/controllers/quantities_controller.rb
+++ app/controllers/quantities_controller.rb
@@ -1,10 +1,15 @@
 class QuantitiesController < ApplicationController
   def create
-    @product = Product.find(params[:product_id])
-    @quantity = Quantity.new(quantity_params)
-    @quantity.product = @product
     @shopping_list = ShoppingList.find_by(mark_as_done: false, user: current_user) || ShoppingList.create(user: current_user, date: Date.today, mark_as_done: false)
-    @quantity.shopping_list = @shopping_list
+    @product = Product.find(params[:product_id])
+    if @shopping_list.products.include?(@product)
+      @quantity = @shopping_list.quantities.find_by(product: @product)
+      @quantity.increment!(:quantity)
+    else
+      @quantity = Quantity.new(quantity_params)
+      @quantity.product = @product
+      @quantity.shopping_list = @shopping_list
+    end
     if @quantity.save
       redirect_to product_path(@product)
     else
@@ -21,6 +26,9 @@ class QuantitiesController < ApplicationController
     else
       render 'pages/home'
     end
+  end
+
+  def destroy
   end
 
   private
