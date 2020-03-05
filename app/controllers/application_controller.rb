5d599ed1d4f9bbefacb28805c0211c5fe559d261
Magda Cembor <magda.cembor@gmail.com>
2020-03-05 15:30:28 +0000

    checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :permit_params, if: :devise_controller?
   before_action :set_shopping_list, if: :user_signed_in?
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   def permit_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :avatar])
