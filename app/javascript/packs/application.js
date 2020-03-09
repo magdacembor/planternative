651650f5d05842ced339577a9bf2e014a0a7946d
Katrina Au <katrinaau95@gmail.com>
2020-03-09 15:18:06 +0000

    chatroom scroll

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,4 +8,13 @@ import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
+  const messageChatroom = document.getElementById("message_content");
+  if (messageChatroom) {
+    messageChatroom.addEventListener('keydown', (e) => {
+        if (e.keyCode === 13) {
+          e.preventDefault();
+          document.getElementById('new_message').submit();
+        }
+    });
+  }
 })
