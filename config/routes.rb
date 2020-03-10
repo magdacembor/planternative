369ce376d3a72b7ab8e87b0889fd20ce4a132d5e
Magda Cembor <magda.cembor@gmail.com>
2020-03-10 16:26:19 +0000

    link to query added

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
     resources :messages, only: :create
   end
   resources :meals, only: :index
-  get '/:nickname', to: 'pages#myprofile', as: :myprofile
   patch 'shopping_lists/:id/mark_as_done', to: 'shopping_lists#mark_as_done', as: :mark_as_done
+  get '/:nickname', to: 'pages#myprofile', as: :myprofile
 end
