2888bcf9612ae49c1a1c7262f612d7b450745a7a
Magda Cembor <magda.cembor@gmail.com>
2020-03-11 14:55:03 +0000

    preview

--- test/mailers/previews/shopping_list_mailer_preview.rb
+++ test/mailers/previews/shopping_list_mailer_preview.rb
@@ -3,7 +3,7 @@ class ShoppingListMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/shopping_list_mailer/shopping_list
   def shopping_list
-    ShoppingListMailer.shopping_list
+    ShoppingListMailer.with(shopping_list: ShoppingList.last).shopping_list
   end
 
 end
