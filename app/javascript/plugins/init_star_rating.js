afbe5949cba61ce1f021d63528c2cba41604c21f
Katrina Au <katrinaau95@gmail.com>
2020-03-11 17:56:07 +0000

    adjust star rating to font awesome style

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -1,10 +1,10 @@
 import "jquery-bar-rating";
-import "jquery-bar-rating/dist/themes/css-stars.css";
+import "jquery-bar-rating/dist/themes/fontawesome-stars.css";
 import $ from 'jquery';
 
 const initStarRating = () => {
   $('#review_rating').barrating({
-    theme: 'css-stars'
+    theme: 'fontawesome-stars'
   });
 };
 
